91496d293dc1e503225f3bccc81adb7dfeab41a4
zyg <2633808836@qq.com>
2024-04-15 09:07:46 +0800

    feat 鉴权框架骨架

---
+++ y-club-auth/y-club-auth-starter/src/main/java/com/yclub/Main.java
@@ -0,0 +1,12 @@
+package com.yclub;
+
+/**
+ * @desc:
+ * @author: zyg
+ * @date: 2024-04-15  8:20
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

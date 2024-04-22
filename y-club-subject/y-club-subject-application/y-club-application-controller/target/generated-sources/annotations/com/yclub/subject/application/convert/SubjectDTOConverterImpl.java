7256a973e56e35af5c12272d0db79ef350ff028e
zyg <2633808836@qq.com>
2024-04-22 16:43:49 +0800

    feat jenkins debug

--- y-club-subject/y-club-subject-application/y-club-application-controller/target/generated-sources/annotations/com/yclub/subject/application/convert/SubjectDTOConverterImpl.java
+++ y-club-subject/y-club-subject-application/y-club-application-controller/target/generated-sources/annotations/com/yclub/subject/application/convert/SubjectDTOConverterImpl.java
@@ -10,7 +10,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-04-22T16:35:10+0800",
+    date = "2024-04-22T16:42:07+0800",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_212 (Oracle Corporation)"
 )
 public class SubjectDTOConverterImpl implements SubjectDTOConverter {
